10b09db5b7daa8449bc9a0aff82ef3f2471fcb95
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-27 18:49:13 -0300

    addd absenceRequestList and absenceRequest

---
+++ src/absence_requests/absenceRequest.jsx
@@ -0,0 +1,10 @@
+export const AbsenceRequest = ({id, start_date, end_date, status}) => {
+  return (
+    <div>
+      <p>{id}</p>
+      <p>{start_date}</p>
+      <p>{end_date}</p>
+      <p>{status}</p>
+    </div>
+  )
+};
--- src/employees/employeeItem.jsx
+++ src/employees/employeeItem.jsx
@@ -1,3 +1,4 @@
+import { AbsenceRequestList } from "../absence_requests/absenceRequestsList";
 import { RemoveEmployee } from "./removeEmployee";
 
 export const EmployeeItem = ({name, email, id}) => {
@@ -6,6 +7,7 @@ export const EmployeeItem = ({name, email, id}) => {
       <h2>{name}</h2>
       <p>{email}</p>
       <p>{id}</p>
+      <AbsenceRequestList id={id} />
       <RemoveEmployee id={id} />
     </div>
   )
