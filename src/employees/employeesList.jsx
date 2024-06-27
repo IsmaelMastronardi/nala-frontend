f29473c8a9fbd60e5f344be5e4bba5afdff2b022
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-27 17:34:29 -0300

    add email filter to employeeList

--- src/employees/employeesList.jsx
+++ src/employees/employeesList.jsx
@@ -1,31 +1,42 @@
 import { useQuery } from "react-query";
-import { Employee } from "./employee";
 import { useState } from "react";
-import { fetchEmployees } from "../api";
+import { fetchEmployees } from "../api/employees/fetchEmployees";
+import { Employee } from "./employee";
 
 export const EmployeesList = () => {
   const [nameFilter, setNameFilter] = useState("");
+  const [emailFilter, setEmailFilter] = useState("");
   const [page, setPage] = useState(1);
+
   const {status, error, data} = useQuery(
-    ["employees", nameFilter],
-    () => fetchEmployees(page, nameFilter),
+    ["employees",page, nameFilter, emailFilter],
+    () => fetchEmployees(page, nameFilter, emailFilter),
+    {retry: false}
   );
-  if(status === 'success'){console.log(data)};
+
   return(
     <div>
       <h1>Employees</h1>
+      <div>
       <input
         type="text"
         value={nameFilter}
         onChange={(e) => setNameFilter(e.target.value)}
         placeholder="Filter by name"
       />
+      <input
+        type="text"
+        value={emailFilter}
+        onChange={(e) => setEmailFilter(e.target.value)}
+        placeholder="Filter by email"
+      />
+      </div>
       {status === 'loading' && <div>Loading...</div>}
       {status === 'error' && <div>Error: {error.message}</div>}
       {status === 'success' && (
         <ul>
           {data.employees.map((employee) => (
-            <li key={employee.id}>{employee.name}</li>
+            <Employee key={employee.id} {...employee} />
           ))}
         </ul>
       )}
