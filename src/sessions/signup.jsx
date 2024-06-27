e48fa4eafc0ea1413448b434f0f900c10a0367a8
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-06-27 16:07:35 -0300

    add signup page - ad signup api call function

---
+++ src/sessions/signup.jsx
@@ -0,0 +1,45 @@
+import { useQuery } from "react-query";
+import { signup } from "../api";
+import { useState } from "react";
+
+
+export const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {status, error, data, refetch} = useQuery(
+    ["signup"],
+    () => signup(email, password),
+    {
+      retry: false,
+      enabled: false
+    }
+  );
+
+  const onClick = () => {
+    if(email && password){
+      refetch();
+    }
+  };
+
+  return(
+    <div>
+    <p>Signup</p>
+    {error && <p>Error: {error.message}</p>}
+    <button onClick={() => onClick()}>SIGNUP BUTTON</button>
+    <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+      />
+      <input
+        type="pasword"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Enter your password"
+      />
+    <a href="/login">Login</a>
+  </div>
+  )
+};
